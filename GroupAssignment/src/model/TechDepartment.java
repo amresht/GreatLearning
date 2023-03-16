f8f77105419811ce1204feaeb129997a4d5f09f4
Amresh Tripathi <amresh2k@gmail.com>
2023-03-16 16:39:10 +0530

    GroupAssignment

---
+++ GroupAssignment/src/model/TechDepartment.java
@@ -0,0 +1,27 @@
+/**
+ * @class           Tech department  Class 
+ * @author          Amresh Tripathi
+ * @date            16-Mar-2023
+ * @version         1.0
+ */
+
+package model;
+
+public class TechDepartment {
+	public static String departmentName() {
+		return "Tech Department";
+	}
+	
+	public static String getTodaysWork() {
+		return "Complete coding of module 1";
+		
+	}
+	public static String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	
+	public static String getTechStackInformation() {
+		return "core Java";
+	}
+}
+
